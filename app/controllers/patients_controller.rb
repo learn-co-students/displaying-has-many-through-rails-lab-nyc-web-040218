ee83923848c7804384fe2ed73ecaaa967ecabac0
github username <github email address>
2018-05-03 10:03:36 -0400

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,16 @@
+class PatientsController < ApplicationController
+  before_action :get_patient, only: :show
+
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+  end
+
+  private
+
+  def get_patient
+    @patient = Patient.find(params[:id])
+  end
+end
